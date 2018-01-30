737c0fbfd768d99a954e14282798fc2075272eed
idibidiart <marc.fawzi@gmail.com>
2018-01-30 12:42:55 -0800

    reverting import to require; node 9.x not queit ready

--- defaultLogger.js
+++ defaultLogger.js
@@ -1,6 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 /* eslint-disable no-console */
-export default {
-  info: msg => console.info(msg),
-  warn: msg => console.warn(msg),
-  error: msg => console.error(msg)
+exports.default = {
+  info: function info(msg) {
+    return console.info(msg);
+  },
+  warn: function warn(msg) {
+    return console.warn(msg);
+  },
+  error: function error(msg) {
+    return console.error(msg);
+  }
 };
